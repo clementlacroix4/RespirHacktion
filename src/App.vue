91253404021c2edb033b6b9e2c8f64c3511d0751
Clement <clement.lacroix@epitech.eu>
2018-12-01 16:10:07 +0100

    [Front] First navbar simplist

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
   <div id="app">
+    <b-navbar toggleable="md" variant="info">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item href="#" class="ml-4">Accueil</b-nav-item>
+        </b-navbar-nav>
+        <!-- Center items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href='#'>LOGO</b-nav-item>
+        </b-navbar-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href='#' class="mr-4" right>Mon compte</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <router-view/>
   </div>
 </template>
@@ -17,6 +33,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.navbar-light .navbar-nav .nav-link {
+    color: rgba(255, 255, 255, 1);
 }
 </style>
